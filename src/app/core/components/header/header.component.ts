5356dc8cf30b46057e201ea4d552a346742a6c8d
Pedro Henrique <pedrohenriqueb366@gmail.com>
2024-07-05 21:01:03 -0300

    feat: including api request

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, inject} from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import { finalize } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
+  nome = '';
+  email = '';
+  telefone = '';
+  cidade = '';
+  httpClient = inject(HttpClient)
+
+  // FUNÇÃO POST API
+  public sendForm(){
+
+    // SETANDO CABEÇALHO DA REQUISIÇÃO (IDENTIFICA PARA O CONTENT-TYPE QUE ESTÁ ENVIANDO UMA APLICAÇÃO JSON E DEPOIS CONFIRMA QUE É UMA APLICAÇÃO JSON)
+    const headers = new HttpHeaders().set('Content-Type', 'application/json')
+    .set('Accept', 'application/json');
+                         //API URL            // BODY
+    this.httpClient.post(environment.apiUrl, JSON.stringify({
+     data: [
+        {
+          nome: this.nome,
+          email: this.email,
+          telefone: this.telefone,
+          cidade: this.cidade,
+        }
+      ]
+  }), {headers: headers})
+  // CONFIGURAÇÃO 'FINALIZE()', ADICIONA UMA FUNÇÃO PARA LIMPAR O FORMULÁRIO APÓS O ENVIO DE DADOS.
+  .pipe(finalize(() => { this.clearForm() }))
+  .subscribe()
+  }
+
+  // FUNÇÃO PARA LIMPAR O FORMULÁRIO E ENVIAR UMA MENSAGEM
+  private clearForm() {
+    this.nome = '';
+    this.email = '';
+    this.telefone = '';
+    this.cidade = '';
+    alert('Registro enviado com sucesso!')
+  }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  apiUrl: 'https://sheetdb.io/api/v1/5h18hkbcf50h9',
+};
