57e0c70ef92af694047d9e4fe589afa1c85d5994
Pedro Henrique <pedrohenriqueb366@gmail.com>
2024-07-09 18:58:01 -0300

    fix: adjusting validation

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,8 +1,32 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Component, inject} from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, ViewChild} from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { finalize } from 'rxjs';
-import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormsModule, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
+
+// VALIDANDO O CAMPO 'TELEFONE' PARA RECEBER APENAS NÚMERO
+function numberValidator(): ValidatorFn {
+  return (control: AbstractControl) => {
+    const isNumber = /[0-9]/.test(control.value)
+    const isText = /[a-z]/.test(control.value)
+    if (isNumber && !isText) {
+      return null;
+    }
+    return { numberValidator: true}
+  }
+}
+
+// VALIDANDO O CAMPO 'CIDADE' E 'NOME' PARA RECEBER APENAS STRING
+function stringValidator(): ValidatorFn {
+  return (control: AbstractControl) => {
+    const isNumber = /[0-9]/.test(control.value)
+    const isText = /[a-z]/.test(control.value)
+    if (!isNumber && isText) {
+      return null;
+    }
+    return { stringValidator: true}
+  }
+}
 
 @Component({
   selector: 'app-header',
@@ -11,17 +35,29 @@ import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angu
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit{
+
+  // RECEBENDO O IDENTIFICADOR DOS ELEMENTOS
+  @ViewChild('h1') public title!: ElementRef;
+  @ViewChild('span') public subTitle!: ElementRef;
+  @ViewChild('btn') public button!: ElementRef;
+
+  // DEFININDO OS VALORES PARA OS ELEMENTOS DINÂMICAMENTE
+  ngAfterViewInit(): void {
+      this.title.nativeElement.innerHTML = 'A <b>Corretora SL91</b> é a corretora exclusiva Hapvida NDI que mais cresce e inova no país.';
+      this.subTitle.nativeElement.innerHTML = 'Junte-se a nós e usufrua do <b>melhor comissionamento</b> do mercado';
+      this.button.nativeElement.innerHTML = 'Entre em contato com nosso suporte';
+  }
 
   // CRIANDO UMA PROPRIEDADE PRIVADA DO FORMBUILDER
   #fb = inject(FormBuilder)
 
   // VARIÁVEIS FORMULÁRIO
   public formHome = this.#fb.group({
-    nome: ['', [Validators.minLength(5), Validators.required]],
+    nome: ['', [Validators.required, Validators.minLength(3), stringValidator()]],
     email: ['', [Validators.required, Validators.email]],
-    telefone: ['', [Validators.required, Validators.minLength(8)]],
-    cidade: ['', [Validators.required, Validators.minLength(3)]],
+    telefone: ['', [Validators.required, Validators.minLength(10), numberValidator()]],
+    cidade: ['', [Validators.required, Validators.minLength(3), stringValidator()]],
   })
 
   httpClient = inject(HttpClient)
