8be2305667287c63a91e9d2a753c6d3856e6a694
Pedro Henrique <pedrohenriqueb366@gmail.com>
2024-07-21 17:53:31 -0300

    fix: adjust button

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -9,6 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-  public srcLogo: string = 'assets/img/logoColor.webp'
-  public altLogo: string = 'imagem do logo colorido'
+  public srcLogo: string = 'assets/img/logoColor.webp';
+  public altLogo: string = 'imagem do logo colorido';
+  public titleRedes: string = 'Siga nossas redes sociais'
+
+  public instaCorretora: string = 'Corretora SL91';
+  public instaJeito: string = 'Jeito SL91';
+  public instCast: string = 'SL Cast';
 }
