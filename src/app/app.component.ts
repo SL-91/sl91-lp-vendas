d4204d065d205344d9a9c23ae4594517f21691b8
Pedro Henrique <pedrohenriqueb366@gmail.com>
2024-07-23 10:50:41 -0300

    add swiper library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./features/home/home.component";
 
@@ -9,6 +9,7 @@ import { HomeComponent } from "./features/home/home.component";
     template: `
     <app-home></app-home>
     `,
+    schemas: [CUSTOM_ELEMENTS_SCHEMA,],
 })
 export class AppComponent {
   title = 'SL91';
--- src/app/core/components/depoimentos/depoimentos.component.ts
+++ src/app/core/components/depoimentos/depoimentos.component.ts
@@ -1,14 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, OnInit, signal, ViewChild } from '@angular/core';
 import { BoxPessoasComponent } from './box-pessoas/box-pessoas.component';
+import { SwiperContainer } from 'swiper/element';
+import { SwiperOptions } from 'swiper/types';
+import { register as registerSwiperElements} from 'swiper/element/bundle';
 
-
+registerSwiperElements();
 @Component({
   selector: 'app-depoimentos',
   standalone: true,
   imports: [BoxPessoasComponent],
   templateUrl: './depoimentos.component.html',
-  styleUrl: './depoimentos.component.scss'
+  styleUrl: './depoimentos.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA,]
 })
-export class DepoimentosComponent {
+
+export class DepoimentosComponent implements OnInit{
+
   public titleDepoimentos: string = 'Confira alguns depoimentos de corretores parceiros';
+
+  swiperElement = signal<SwiperContainer | null>(null);
+
+  configSwiper() {
+    const swiperElementConstructor = document.querySelector('swiper-container');
+    const swiperOptions: SwiperOptions = {
+      slidesPerView: 1,
+      slideToClickedSlide: true,
+      pagination: {
+        enabled: true,
+        clickable: true,
+        dynamicBullets: true
+      },
+      navigation: {
+        enabled: true,
+      },
+      speed: 1000,
+      injectStyles: [
+        `
+          .swiper-button-prev svg {
+            width: 15px;
+            color: var(--primary);
+          }
+          .swiper-button-next svg {
+            width: 15px;
+            color: var(--primary);
+          }
+          .swiper-pagination-bullet {
+            background: var(--primary);
+          }
+        `,
+      ],
+    };
+
+    Object.assign(swiperElementConstructor!, swiperOptions)
+    this.swiperElement.set(swiperElementConstructor as SwiperContainer);
+    this.swiperElement()?.initialize()
+  }
+
+  ngOnInit(): void {
+      this.configSwiper()
+  }
+
 }
